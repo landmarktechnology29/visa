b33688ca1e8fec5ddd4dd1b95aaba8970026b270
Adrian Mbaku <mbakuan@yahoo.com>
2022-08-28 18:41:57 -0400

    deploying in dev

--- deploy.py
+++ deploy.py
@@ -1,6 +1,7 @@
 #!/bin/python
-#Deploying a python web application
+Deploying a python web application
 echo "Hello World"
-# shell Script
-python ('Hello World') 
-#python scrip
+shell Script
+print ('Hello World') 
+:python scripi
+deploy in the dev environment
