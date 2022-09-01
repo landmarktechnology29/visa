ebb377d6cd7daaa57783275dfe7e2905d78f78ea
Adrian Mbaku <mbakuan@yahoo.com>
2022-08-31 20:29:59 -0400

    tested and approved conflict resolved

--- deploy.py
+++ deploy.py
@@ -3,6 +3,13 @@ Deploying a python web application
 echo "Hello World"
 shell Script
 print ('Hello World') 
-:python scripi
+python scripi
 deploy in the dev environment
 deployment5 master
+HEAD
+master
+deploy to staging
+good job
+deployment successful
+echo tested and approve by client good job
+2e642cf (tested and approved)
